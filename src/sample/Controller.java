303579a7d46bc13fc14f84acd5760730f2352bdb
InSantr <alekskom2004@gmail.com>
2016-02-03 21:59:54 +0200

    no message

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,6 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -8,11 +9,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable, ChangeListener {
+public class Controller implements Initializable, ChangeListener, SerialPortEventListener {
+
+    private static SerialPort serialPort;
 
     @FXML
     private TextArea txt2;
@@ -48,40 +55,88 @@ public class Controller implements Initializable, ChangeListener {
         ail_slider.setValue(255/2);
         ele_slider.setValue(255/2);
         rud_slider.setValue(255/2);
-        t_ail_slider.setValue(255/2);
-        t_ele_slider.setValue(255/2);
-        t_rud_slider.setValue(255/2);
+        t_ail_slider.setValue(0x94);
+        t_ele_slider.setValue(0x44);
+        t_rud_slider.setValue(0x21);
+        //0 7F 7F 7F 94 44 21
     }
 
 
     public void click_reconnect(ActionEvent actionEvent) {
-        txt2.appendText("ddd");
+        if(serialPort != null && serialPort.isOpened()) {
+            try {
+                serialPort.closePort();
+            } catch (SerialPortException e) {
+                e.printStackTrace();
+            }
+        }
+        serialPort = new SerialPort("COM3");
+        try {
+            //Открываем порт
+            serialPort.openPort();
+            //Выставляем параметры
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            //Включаем аппаратное управление потоком
+            //serialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN |
+            //        SerialPort.FLOWCONTROL_RTSCTS_OUT);
+            //Устанавливаем ивент лисенер и маску
+            serialPort.addEventListener(this::serialEvent, SerialPort.MASK_RXCHAR);
+        }
+        catch (SerialPortException ex) {
+            System.out.println(ex);
+        }
     }
 
     public void refresh(){
         String res = "";
-        res += String.format("THR: 0x%02X;\t", ((int) thr_slider.getValue()));
-        res += String.format("AIL: 0x%02X;\t", ((int) ail_slider.getValue()));
-        res += String.format("ELE: 0x%02X;\t", ((int) ele_slider.getValue()));
-        res += String.format("RUD: 0x%02X;\t", ((int) rud_slider.getValue()));
-        res += String.format("T_AIL: 0x%02X;\t", ((int) t_ail_slider.getValue()));
-        res += String.format("T_ELE: 0x%02X;\t", ((int) t_ele_slider.getValue()));
+        res += String.format("THR: 0x%02X;     ", ((int) thr_slider.getValue()));
+        res += String.format("AIL: 0x%02X;     ", ((int) ail_slider.getValue()));
+        res += String.format("ELE: 0x%02X;     ", ((int) ele_slider.getValue()));
+        res += String.format("RUD: 0x%02X;     ", ((int) rud_slider.getValue()));
+        res += String.format("T_AIL: 0x%02X;     ", ((int) t_ail_slider.getValue()));
+        res += String.format("T_ELE: 0x%02X;     ", ((int) t_ele_slider.getValue()));
         res += String.format("T_RUD: 0x%02X;\t", ((int) t_rud_slider.getValue()));
         res += String.format("\r\n");
 
         sum_label.setText(res);
-        //txt2.appendText(res);
+
+        if(serialPort == null || !serialPort.isOpened())
+            return;
+
+        byte[] array = new byte[8];
+        array[0] = (byte)((int) thr_slider.getValue());
+        array[1] = (byte)((int) ail_slider.getValue());
+        array[2] = (byte)((int) ele_slider.getValue());
+        array[3] = (byte)((int) rud_slider.getValue());
+        array[4] = (byte)((int) t_ail_slider.getValue());
+        array[5] = (byte)((int) t_ele_slider.getValue());
+        array[6] = (byte)((int) t_rud_slider.getValue());
+        array[7] = 0x00;
+        try {
+            serialPort.writeBytes(array);
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void changed(ObservableValue observable, Object oldValue, Object newValue) {
-//        Object obj = ((DoublePropertyBase) observable).getBean();
-//        if(obj.equals(thr_slider)) {
-//            thr_text.setText(((Number)newValue).intValue() + "");
-//        } else if (obj.equals(thr_text)){
-//            thr_slider.setValue( Integer.parseInt(thr_text.getText()));
-//        }
-
         refresh();
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent serialPortEvent) {
+        if(serialPortEvent.isRXCHAR() && serialPortEvent.getEventValue() > 0){
+            try {
+                String data = serialPort.readString(serialPortEvent.getEventValue());
+                //Platform.runLater(() -> txt2.appendText(data));
+            }
+            catch (SerialPortException ex) {
+                System.out.println(ex);
+            }
+        }
     }
 }
